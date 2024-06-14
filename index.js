018345027debd488379eb1deb9c8386fef397d7b
Matt Eva <matteweva@gmail.com>
2024-06-14 15:32:57 -0700

    handled first text node problem

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // console.log("hello world!");
+
+const editor = document.getElementById("editor");
+
+editor.addEventListener("mousedown", (e) => {
+  const children = editor.childNodes;
+
+  if (children.length > 0 && children[0].nodeName === "#text") {
+    const p = document.createElement("p");
+    p.textContent = children[0].textContent;
+    editor.replaceChild(p, children[0]);
+  }
+});
