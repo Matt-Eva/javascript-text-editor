3c349b42409268d3d159b93df978cf4033a75b7d
Matt Eva <matteweva@gmail.com>
2024-06-17 18:24:06 -0700

    accessing focused element

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // console.log("hello world!");
 
 const editor = document.getElementById("editor");
+let focusedElement;
 
 editor.addEventListener("mousedown", (e) => {
   const children = editor.childNodes;
+  console.log(e.target);
 
   if (children.length > 0 && children[0].nodeName === "#text") {
     const p = document.createElement("p");
     p.textContent = children[0].textContent;
     editor.replaceChild(p, children[0]);
+
+    if (children.length === 1) {
+      focusedElement = p;
+    } else if (e.target.id !== editor.id) {
+      focusedElement = e.target;
+    }
+  } else if (e.target.id !== editor.id) {
+    focusedElement = e.target;
   }
+
+  console.log(focusedElement);
 });
