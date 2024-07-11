9c621938f439e9b4dc6c7c89ab4113a0f9ad7736
Matt Eva <matteweva@gmail.com>
2024-07-11 09:11:51 -0700

    preserving formatting and selection across multiple paragraphs

--- src/handle_formatting.js
+++ src/handle_formatting.js
@@ -4,32 +4,47 @@ import { convertToAccessibleFormatting } from "./convert_formatting_accessible.j
 export async function handleFormatting(state, editor, style) {
   const selection = window.getSelection();
   const range = selection.getRangeAt(0);
-  const fragment = range.cloneContents();
+  let startContainer = range.startContainer;
+  let startOffset = range.startOffset;
+  let endContainer = range.endContainer;
+  let endOffset = range.endOffset;
+  const fragment = range.extractContents();
+  const fragmentChildren = fragment.childNodes;
   const rangeChildren = [];
   console.log("fragment children", fragment.childNodes);
-  for (const child of fragment.childNodes) {
-    const newNode = document.createElement("em");
-    if (child.nodeName === "BR") {
-      rangeChildren.appendChild(child);
-    } else if (child.childNodes.length !== 0) {
-      const nestedChildren = [...child.childNodes];
-      for (const nestedChild of nestedChildren) {
-        newNode.appendChild(nestedChild);
-      }
-      rangeChildren.push(newNode);
-    } else if (child.textContent !== "") {
-      newNode.textContent = child.textContent;
-      rangeChildren.push(newNode);
+  for (let i = 0; i < fragmentChildren.length; i++) {
+    const fragmentChild = fragmentChildren[i];
+    console.log("fragmentChild", fragmentChild);
+    const range = new Range();
+    const em = document.createElement("em");
+    if (i === 0) {
+      range.setStart(startContainer, startOffset);
+      em.textContent = fragmentChild.textContent;
+      range.insertNode(em);
+      startContainer = em;
+    } else if (i === fragmentChildren.length - 1) {
+      console.log("fragment child length", fragmentChild.length);
+      endOffset = fragmentChild.textContent.length - 1;
+      range.setStart(endContainer, 0);
+      range.setEnd(endContainer, endOffset);
+      em.textContent = fragmentChild.textContent;
+      range.insertNode(em);
+      endContainer = em;
+    } else {
     }
   }
-  console.log("range children", rangeChildren);
+  // console.log("range children", rangeChildren);
 
-  while (rangeChildren.length !== 0) {
-    const lastNode = rangeChildren.pop();
-    range.insertNode(lastNode);
-  }
+  // while (rangeChildren.length !== 0) {
+  //   const lastNode = rangeChildren.pop();
+  //   range.insertNode(lastNode);
+  // }
   // console.log("start container parent", range.startContainer);
   // console.log("end container parent", range.endContainer);
+  console.log("endContainer", endContainer);
+  range.setStart(startContainer, 0);
+  range.setEnd(endContainer, endOffset);
+  console.log("final range", range);
 
   selection.removeAllRanges();
   selection.addRange(range);
